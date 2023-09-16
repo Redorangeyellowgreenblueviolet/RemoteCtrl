363064fbdff0b1489b8e7bc379d1daaa5d734f89
dongyuqiwork <dongyuqi.work@foxmail.com>
2023-09-16 22:55:04 +0800

    1.通过CopyFile程序到startup中实现开机启动

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -23,13 +23,57 @@
 
 CWinApp theApp;
 
+//开机启动时，程序的权限跟随启动用户的，当两者权限不同时，通常程序启动失败
+//开机启动对环境变量有影响，如果依赖动态库，可能启动失败
+//复制dll到system32或者SysWOW64下，或者采用静态库
+void WriteRegisterTable(const CString& strPath) {
+
+    CString strSubKey = _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+    char sPath[MAX_PATH] = "";
+    char sSys[MAX_PATH] = "";
+    std::string strExe = "\\RemoteCtrl.exe ";
+    GetCurrentDirectoryA(MAX_PATH, sPath);
+    GetSystemDirectoryA(sSys, MAX_PATH);
+    std::string strCmd = "mklink " + std::string(sSys) + strExe + std::string(sPath) + strExe;
+    int ret = system(strCmd.c_str());
+    TRACE("system %d\r\n", ret);
+    HKEY hKey = NULL;
+    ret = RegOpenKeyEx(HKEY_LOCAL_MACHINE, strSubKey, 0, KEY_ALL_ACCESS | KEY_WOW64_64KEY, &hKey);
+    if (ret != ERROR_SUCCESS) {
+        RegCloseKey(hKey);
+        MessageBox(NULL, _T("设置自启开机启动失败\r\n程序启动失败"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
+        exit(0);
+    }
+
+    ret = RegSetValueEx(hKey, _T("RemoteCtrl"), 0, REG_EXPAND_SZ, (BYTE*)(LPCTSTR)strPath, strPath.GetLength() * sizeof(TCHAR));
+    if (ret != ERROR_SUCCESS) {
+        RegCloseKey(hKey);
+        MessageBox(NULL, _T("设置自启开机启动失败\r\n程序启动失败"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
+        exit(0);
+    }
+    RegCloseKey(hKey);
+}
+
+void WriteStartupDir(const CString& strPath) {
+    
+    CString strCmd = GetCommandLine();
+    strCmd.Replace(_T("\""), _T(""));
+    //fopen CFile system(copy) CopyFile
+    bool ret = CopyFile(strCmd, strPath, FALSE);
+    if (ret == false) {
+        MessageBox(NULL, _T("复制文件失败"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
+        exit(0);
+    }
+}
+
+
 void ChooseAutoInvoke() {
-    CString strPath = CString(_T("C:\\Windows\\SysWOW64\\RemoteCtrl.exe"));
+    //CString strPath = CString(_T("C:\\Windows\\SysWOW64\\RemoteCtrl.exe"));
+    CString strPath = _T("C:\\Users\\21843\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\RemoteCtrl.exe");
     if (PathFileExists(strPath)) {
         return;
     }
 
-    CString strSubKey = _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
     CString strInfo = _T("该程序只允许用于合法的用途\n");
     strInfo += _T("进入被监控状态\n");
     strInfo += _T("按下“是”将随系统启动而自动运行\n");
@@ -37,30 +81,8 @@ void ChooseAutoInvoke() {
     strInfo += _T("按下“取消”将退出\n");
     int ret = MessageBox(NULL, strInfo, _T("警告"), MB_YESNOCANCEL | MB_ICONWARNING | MB_TOPMOST);
     if (ret == IDYES) {
-        char sPath[MAX_PATH] = "";
-        char sSys[MAX_PATH] = "";
-        std::string strExe = "\\RemoteCtrl.exe ";
-        GetCurrentDirectoryA(MAX_PATH, sPath);
-        GetSystemDirectoryA(sSys, MAX_PATH);
-        std::string strCmd = "mklink " + std::string(sSys) + strExe + std::string(sPath) + strExe;
-        ret = system(strCmd.c_str());
-        TRACE("system %d\r\n", ret);
-        HKEY hKey = NULL;
-        ret = RegOpenKeyEx(HKEY_LOCAL_MACHINE, strSubKey, 0, KEY_ALL_ACCESS | KEY_WOW64_64KEY, &hKey);
-        if (ret != ERROR_SUCCESS) {
-            RegCloseKey(hKey);
-            MessageBox(NULL, _T("设置自启开机启动失败\r\n程序启动失败"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
-            exit(0);
-        }
-        
-        ret = RegSetValueEx(hKey, _T("RemoteCtrl"), 0, REG_EXPAND_SZ, (BYTE*)(LPCTSTR)strPath, strPath.GetLength() * sizeof(TCHAR));
-        if (ret != ERROR_SUCCESS) {
-            RegCloseKey(hKey);
-            MessageBox(NULL, _T("设置自启开机启动失败\r\n程序启动失败"), _T("错误"), MB_ICONERROR | MB_TOPMOST);
-            exit(0);
-        }
-        RegCloseKey(hKey);
-
+        //WriteRegisterTable(strPath);
+        WriteStartupDir(strPath);
     }
     else if (ret == IDCANCEL) {
         exit(0);
